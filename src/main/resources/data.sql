86a9f8732ccdd613af48e42471aab5ca1529dbb4
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-10 21:01:20 +0900

    chore: sql init never 설정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,6 @@ VALUES (1, 'SEOUL', 'KOREA', '원티드랩');
 INSERT INTO job_posting (job_posting_id, compensation, posting_position, posting_details,
                          technology_used, company_id)
 VALUES (1, 1000000, '백엔드 주니어 개발자', '원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..', 'Python', 1);
+
+INSERT INTO members (members_id, name)
+VALUES (1, '이정우')
