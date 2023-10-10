e40f1f78521403ccab796efbcdb6053bdc7b05cb
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-10 21:07:52 +0900

    test: 채용 공고 리스트 조회 기능 테스트

--- src/test/java/com/wnated/findjob/api/service/JobPostingServiceTest.java
+++ src/test/java/com/wnated/findjob/api/service/JobPostingServiceTest.java
@@ -11,6 +11,7 @@ import com.wnated.findjob.dto.jobposting.requeset.JobUpdateRequest;
 import com.wnated.findjob.dto.jobposting.response.JobResponse;
 import com.wnated.findjob.repository.company.CompanyRepository;
 import com.wnated.findjob.repository.jobposting.JobPostingRepository;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -41,7 +42,6 @@ class JobPostingServiceTest {
     void createJobPosting() {
         //given
         Company company = createCompany();
-
         Company savedCompany = companyRepository.save(company);
 
         JobCreateRequest request = JobCreateRequest.builder()
@@ -64,7 +64,6 @@ class JobPostingServiceTest {
     void updateJob() {
         //given
         Company company = createCompany();
-
         Company savedCompany = companyRepository.save(company);
 
         JobCreateRequest request = JobCreateRequest.builder()
@@ -91,6 +90,46 @@ class JobPostingServiceTest {
 
         //then
         assertThat(findJobPosting.getCompensation()).isEqualTo(1000000);
+    }
+
+    @DisplayName("채용 공고 리스트를 조회 한다.")
+    @Test
+    void findJobPostings() {
+        //given
+        Company company = createCompany();
+        Company savedCompany = companyRepository.save(company);
+
+        JobPosting request1 = JobPosting.builder()
+            .company(savedCompany)
+            .position("백엔드")
+            .postingDetails("백엔드 개발자 채용합니다.")
+            .compensation(500000)
+            .technologyUsed("Java")
+            .build();
+
+        JobPosting request2 = JobPosting.builder()
+            .company(savedCompany)
+            .position("프론트 엔드")
+            .postingDetails("프론트 엔드 개발자 채용합니다.")
+            .compensation(100000)
+            .technologyUsed("React")
+            .build();
+
+        JobPosting request3 = JobPosting.builder()
+            .company(savedCompany)
+            .position("파이썬")
+            .postingDetails("파이썬 개발자 채용합니다.")
+            .compensation(300000)
+            .technologyUsed("Python")
+            .build();
+
+        jobPostingRepository.saveAll(List.of(request1, request2, request3));
+
+        //when
+        List<JobResponse> jobs = jobPostingService.findJobs();
+
+        //then
+        assertThat(jobs).hasSize(3);
     }
 
     private static Company createCompany() {
