514ac5fbc9a21885c7b49ca50048f5e7ebb64c6d
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-10-12 00:48:55 +0900

    test: 채용 공고 지원 기능 테스트

--- src/test/java/com/wanted/findjob/api/service/ApplyServiceTest.java
+++ src/test/java/com/wanted/findjob/api/service/ApplyServiceTest.java
@@ -1,15 +1,20 @@
 package com.wanted.findjob.api.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.wanted.findjob.domain.company.City;
+import com.wanted.findjob.domain.company.Company;
+import com.wanted.findjob.domain.company.Country;
 import com.wanted.findjob.domain.jobposting.JobPosting;
+import com.wanted.findjob.domain.user.ApplyHistory;
 import com.wanted.findjob.domain.user.User;
 import com.wanted.findjob.dto.userjobposting.request.ApplyCompanyRequest;
 import com.wanted.findjob.repository.applyHistory.ApplyHistoryRepository;
 import com.wanted.findjob.repository.company.CompanyRepository;
 import com.wanted.findjob.repository.jobposting.JobPostingRepository;
 import com.wanted.findjob.repository.user.UserRepository;
-import com.wanted.findjob.domain.company.City;
-import com.wanted.findjob.domain.company.Company;
-import com.wanted.findjob.domain.company.Country;
+import java.util.ArrayList;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,14 +51,15 @@ class ApplyServiceTest {
         companyRepository.deleteAllInBatch();
     }
 
+    @Transactional
     @DisplayName("사용자는 채용 공고를 지원 할 수 있다.")
     @Test
-    @Transactional
     void applyCompany() {
         //given
         User user = User.builder()
             .id(1L)
             .name("jw")
+            .applyHistory(new ArrayList<ApplyHistory>())
             .build();
 
         User savedUser = userRepository.save(user);
@@ -83,8 +89,48 @@ class ApplyServiceTest {
         applyService.applyCompany(request);
 
         //then
+        assertThat(savedUser.getApplyHistory()).hasSize(1);
+    }
 
+    @Transactional
+    @DisplayName("사용자가 지원했던 채용공고에 다시 지원하면 예외를 발생 시킨다.")
+    @Test
+    void reapplyCompany() {
+        //given
+        User user = User.builder()
+            .id(1L)
+            .name("jw")
+            .applyHistory(new ArrayList<ApplyHistory>())
+            .build();
 
+        User savedUser = userRepository.save(user);
+
+        Company company = Company.builder()
+            .name("company1")
+            .country(Country.KOREA)
+            .city(City.SEOUL)
+            .build();
+        Company savedCompany = companyRepository.save(company);
+
+        JobPosting jobPosting = JobPosting.builder()
+            .company(savedCompany)
+            .position("백엔드")
+            .postingDetails("백엔드 개발자 채용합니다.")
+            .compensation(500000)
+            .technologyUsed("Java")
+            .build();
+
+        JobPosting savedJobPosting = jobPostingRepository.save(jobPosting);
+
+        ApplyCompanyRequest request = new ApplyCompanyRequest();
+        request.setUserId(savedUser.getId());
+        request.setJobPostingId(savedJobPosting.getId());
+
+        //when
+        applyService.applyCompany(request);
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> applyService.applyCompany(request));
     }
 
 }
